855b3c77c2c05f1740b5f668304554a5e32b537b
Arthur <105047736+arthur-rule@users.noreply.github.com>
2024-01-24 23:22:12 +0300

    01/24/2024

--- src/test/java/com/test/stepdefinitions/Login_stepdefinitions.java
+++ src/test/java/com/test/stepdefinitions/Login_stepdefinitions.java
@@ -1,4 +1,19 @@
 package com.test.stepdefinitions;
 
 public class Login_stepdefinitions {
+
+
+    GoogleSearchPage googleSearchPage = new GoogleSearchPage();
+
+    @When("user searches for {word}")
+    public void user_searches_for_apple(String word) {
+
+        googleSearchPage.searchBox.sendKeys(word + Keys.ENTER);
+
+    }
+
+    @Then("user should see {word} in the title")
+    public void user_should_see_apple_in_the_title(String word) {
+
+        BrowserUtils.verifyTitle(word+" - Google Search");
 }
