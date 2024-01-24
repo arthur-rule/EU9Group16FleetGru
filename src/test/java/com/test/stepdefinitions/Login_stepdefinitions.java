bda72c6e2cb63698fd9ddd172076ffc45d81a603
Arthur <105047736+arthur-rule@users.noreply.github.com>
2024-01-24 23:03:10 +0300

    01/24/2024

--- src/test/java/com/test/stepdefinitions/Login_stepdefinitions.java
+++ src/test/java/com/test/stepdefinitions/Login_stepdefinitions.java
@@ -1,4 +1,39 @@
 package com.test.stepdefinitions;
 
 public class Login_stepdefinitions {
+
+    BingSearchPage bingSearchPage = new BingSearchPage();
+
+    @Given("user is on the Bing search page")
+    public void user_is_on_the_bing_search_page() {
+
+        Driver.getDriver().get("https://www.bing.com");
+
+    }
+    @When("user enters orange in the Bing search box")
+    public void user_enters_orange_in_the_bing_search_box() {
+        bingSearchPage.searchBox.sendKeys("orange" + Keys.ENTER);
+    }
+
+
+    @When("user enters {string} in the Bing search box")
+    public void userEntersInTheBingSearchBox(String searchKey) {
+
+        bingSearchPage.searchBox.sendKeys(searchKey + Keys.ENTER);
+
+    }
+
+    @Then("user should see {string} in the title")
+    public void userShouldSeeInTheTitle(String expectedTitle) {
+
+        BrowserUtils.verifyTitle(expectedTitle);
+        //BrowserUtils.verifyTitleContains(expectedTitle);
+
+    }
+
+    @Then("user should see orange in the title")
+    public void userShouldSeeOrangeInTheTitle() {
+        BrowserUtils.verifyTitle("orange - Search");
+
+    }
 }
